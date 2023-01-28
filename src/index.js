f2d8a27f57f815e36486410619e5430b5083e14b
NicRiv <nico.a.rivera3@gmail.com>
2023-01-28 10:16:10 -0300

    main features

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 /**
- * type-str
+ * what-str
  */
+
+// is str an uppercase?
+const isUpper = (str) => {
+
+    return (/^[A-Z]+$/).test(str)
+}
+
+// is str an lowercase?
+const isLower = (str) => {
+
+    return (/^[a-z]+$/).test(str)
+}
+
+// Export
+module.exports = {
+    isUpper, isLower
+}
